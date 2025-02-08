714edd332960869dcdb7a3d13d85b179231d1a39
Axel Delafosse <axeldelafosse@gmail.com>
2025-02-08 12:37:46 +0100

    Upgrade to 2.0.5

--- stemgen/cli.py
+++ stemgen/cli.py
@@ -28,7 +28,7 @@ Usage: stemgen -i [INPUT_PATH] -o [OUTPUT_PATH]
 
 Supported input file format: {SUPPORTED_FILES}
 """
-VERSION = "7.0.0"
+VERSION = "2.0.5"
 
 # Get the package root directory
 PACKAGE_DIR = Path(__file__).parent.absolute()
--- stemgen/stem.py
+++ stemgen/stem.py
@@ -38,7 +38,7 @@ Example: 'track.0.wav' for the master file then 'track.1.wav' for the first stem
 
 You can also use ableton.py to automatically create the stems from Ableton Live.
 """
-VERSION = "2.0.0"
+VERSION = "2.0.5"
 
 INSTALL_DIR = Path(__file__).parent.absolute()
 PROCESS_DIR = os.getcwd()
